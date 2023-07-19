7fbcc828576ab0431e993b4c7af24b3eb039aa59
Hima <abrahimmahmoud5454@gmail.com>
2023-07-19 03:02:38 +0300

    si

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,9 @@ int print_last_digit(int n)
 
 	if (n < 0)
 		n = -n;
-	last_digit = n % 10;
-		_putchar(last_digit + '0');
+		last_digit = n % 10;
+
+	_putchar(last_digit + '0');
 
 
 	return (last_digit);
