37cf6423d49832596e0bb9101aac572acbe40365
Hima <abrahimmahmoud5454@gmail.com>
2023-07-21 00:55:53 +0300

    si

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n => 0)
+	{
+		for (len = 1; len < n; len++)
+			_putchar('95');
+	}
+
+	_putchar('\n');
+}
