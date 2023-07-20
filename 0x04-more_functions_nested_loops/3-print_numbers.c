183bb74b81f310904fd9349577561012a050452c
Hima <abrahimmahmoud5454@gmail.com>
2023-07-21 00:24:58 +0300

    si

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+{
+
+	int n;
+
+
+	for (n = 48 ; n <= 57 ; n++)
+		_putchar(n);
+
+	_putchar('\n');
+}
