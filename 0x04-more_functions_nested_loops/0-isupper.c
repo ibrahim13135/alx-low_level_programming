49b595ee4d7e298da4c24a2f9b416d47cb8adb88
Hima <abrahimmahmoud5454@gmail.com>
2023-07-21 00:02:33 +0300

    si

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,10 @@
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 
-	if (c >= 'A' && c <= 'B')
+	if (c >= 65 && c <= 90)
 		return (0);
 
 	else
