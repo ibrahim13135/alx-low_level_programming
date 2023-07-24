e9a5b6520375f3e68c6bba2bdd5e65da3937f12b
Hima <abrahimmahmoud5454@gmail.com>
2023-07-24 12:55:30 +0300

    si

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
-void _puts(char *str) {
-    printf("%s\n", str);
-}
-
-int main() {
-	char *str = "Hello World";
+void _puts(char *str)
+{
+	char *str;
 	_puts(str);
-	return 0;
+
+	return (*str);
 }
 
