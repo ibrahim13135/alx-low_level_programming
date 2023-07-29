2fb5a91586c25f462d21f0989be4561aaedfa548
Hima <abrahimmahmoud5454@gmail.com>
2023-07-29 05:54:20 +0300

    si

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,7 +5,6 @@
  * @n: integer params
  * Return: 0
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
@@ -14,7 +13,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n1 = -n;
 	}
 
@@ -22,5 +21,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
