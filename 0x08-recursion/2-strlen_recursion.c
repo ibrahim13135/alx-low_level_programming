9c1f3138ea147848d6267ce9a39bebb3f345334a
Hima <abrahimmahmoud5454@gmail.com>
2023-08-03 03:57:42 +0300

    si

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*str != '\0')
+		length++;
+		_strlen_recursion(++str);
+	else
+		return (length);
+
+}
