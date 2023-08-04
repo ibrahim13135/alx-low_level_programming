4e365bcbd3223dc323491b405f7de8f118029ac7
Hima <abrahimmahmoud5454@gmail.com>
2023-08-04 05:54:56 +0300

    si

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
+	int len = 0;
 
-	if (*s != '\0')
-		length++;
-		length += _strlen_recursion(s + 1);
+	if (*s)
 
-		return (length);
+		len++;
+		len += _strlen_recursion(s + 1);
 
+
+	return (len);
 }
