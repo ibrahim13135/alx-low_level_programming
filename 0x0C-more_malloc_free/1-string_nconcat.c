7c40b79692a45db61f8cac8dbf6247ec3891ccfe
Hima <abrahimmahmoud5454@gmail.com>
2023-08-10 18:57:55 +0300

    si

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= len2)
 		n = len2;
-		a = malloc(sizeof(char) * (len1 + len2 + 1));
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (a == NULL)
 		return (NULL);
