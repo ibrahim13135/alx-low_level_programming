db9e51b1d13661bb8fa39819d13e0c20ba6160da
Hima <abrahimmahmoud5454@gmail.com>
2023-08-11 01:14:00 +0300

    si

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,58 +8,43 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
-	char *old_p;
-	unsigned int i, min;
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
 
 	if (new_size == old_size)
-
 		return (ptr);
 
 	if (ptr == NULL)
 	{
-		p = malloc(new_size);
-		if (p == NULL)
+		mem = malloc(new_size);
+
+		if (mem == NULL)
 			return (NULL);
+
+		return (mem);
 	}
 
 	if (new_size == 0 && ptr != NULL)
-
-		free(ptr);
-		return (NULL);
-
-	p = malloc(new_size);
-
-	if (p == NULL)
-		return (NULL);
-
-	old_p = ptr;
-	p = malloc(sizeof(*old_p) * new_size);
-
-	if (p == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
 
-	for (i = 0; i < min; i++)
-		p[i] = *old_p++;
+	if (mem == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	filler = mem;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
 
 	free(ptr);
-	return ((void *)p);
+	return (mem);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
