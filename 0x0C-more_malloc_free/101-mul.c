79e06ba739ae5a7c028679fbcaa46883b96c5559
Hima <abrahimmahmoud5454@gmail.com>
2023-08-11 01:47:11 +0300

    si

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_number - checks if a string is a valid number
+ * @s: the string to check
+ * Return: 1 if true, 0 if false
+ */
+int is_number(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	if (!isdigit(s[i]))
+	{
+		return (0);
+	}
+	}
+
+	return (1);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 if success, 98 if error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (!is_number(argv[1]) || !is_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
