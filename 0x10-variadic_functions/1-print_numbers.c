1f63fec10230aeb880ea4e60f54b483fdaa337ff
Hima <abrahimmahmoud5454@gmail.com>
2023-08-18 04:00:10 +0300

    si

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,12 +22,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 
 		if (separator != NULL && i < n - 1)
-
 			printf("%s", separator);
 
 
-	va_end(args);
-
 
 	printf("\n");
+
+	va_end(args);
 }
