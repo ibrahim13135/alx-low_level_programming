1448c0dc584db9172d9877b67a8da24c69e3d2bf
Hima <abrahimmahmoud5454@gmail.com>
2023-10-17 02:02:51 +0300

    si

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ibrahim13135/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgig.so
-export LO_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://github.com/ibrahim13135/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
